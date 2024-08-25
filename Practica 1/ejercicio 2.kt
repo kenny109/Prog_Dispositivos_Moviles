9c90a9f17d66a4c89883e918060f8673900c390c
Kenny Luis Flores Chacón <kfloresc@ulasalle.edu.pe>
2024-08-25 00:25:03 -0500

    implementando funcion para determinar el resultado del juego y imprimir el resultado

--- Practica 1/ejercicio 2.kt
+++ Practica 1/ejercicio 2.kt
@@ -4,8 +4,25 @@ fun elegirOpcionComputadora(): String {
     val opciones = listOf("Piedra", "Papel", "Tijera")
     return opciones[Random.nextInt(opciones.size)]
 }
+
+fun determinarResultadoJuego(eleccionUser: String, eleccionComputer: String): String{
+    return when{
+        eleccionUser == eleccionComputer -> "Empate"
+        (eleccionUser == "Piedra" && eleccionComputer == "Tijera") ||
+                (eleccionUser == "Papel" && eleccionComputer == "Piedra") ||
+                (eleccionUser == "Tijera" && eleccionComputer == "Papel") -> "Ganaste"
+        else -> "Perdiste"
+    }
+
+}
 fun main (){
     val eleccionComputadora = elegirOpcionComputadora()
-
     println("la computadora eligio: ${eleccionComputadora}")
+
+    println("Elige una opción: Piedra, Papel o Tijera: " )
+    val eleccionUsuario = readLine()?.capitalize() ?: ""
+
+    val resultado = determinarResultadoJuego(eleccionUsuario, eleccionComputadora)
+
+    println("Resultado: $resultado")
 }
