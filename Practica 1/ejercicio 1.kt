11d63b4018d238b73fa496a93bb55098e27080ba
Kenny Luis Flores Chacón <kfloresc@ulasalle.edu.pe>
2024-08-24 23:21:20 -0500

    creando funcion para obtener el nivel secun su puntuacion, y condicional en el main para validar los datos de puntuacion y salario

--- Practica 1/ejercicio 1.kt
+++ Practica 1/ejercicio 1.kt
@@ -1,14 +1,24 @@
 fun calcularDineroRecibido(salarioMensual: Double, puntuacion: Int): Double {
     return salarioMensual * (puntuacion / 10.0)
 }
-
+fun obtenerNivel(puntuacion: Int): String {
+    return when {
+        puntuacion in 0..3 -> "Inaceptable"
+        puntuacion in 4..6 -> "Aceptable"
+        puntuacion in 7..10 -> "Meritorio"
+        else -> "Puntuación inválida"
+    }
+}
 
 fun main(){
     println("Ingrese la puntuación (0 a 10)")
-    val puntuación = readln()?.toIntOrNull() ?:0
+    val puntuacion = readln()?.toIntOrNull() ?:0
 
     println("Ingrese el salario")
     val salarioMensual = readln()?.toDoubleOrNull() ?:0
 
-
+    if (puntuacion == null || puntuacion !in 0..10 || salarioMensual == null || salarioMensual <= 0) {
+        println("Datos inválidos. Asegúrese de que la puntuación esté entre 0 y 10 y el salario sea positivo.")
+        return
+    }
 }
