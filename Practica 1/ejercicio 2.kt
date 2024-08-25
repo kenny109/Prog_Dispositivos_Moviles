fa2ab61afabfc52c34ff251b5b9a17c7dc5a989c
Kenny Luis Flores Chacón <kfloresc@ulasalle.edu.pe>
2024-08-25 00:03:18 -0500

    probando funcion random para la eleccion de la computadora haciendo que se imprima para ver si funciona correctamente

--- Practica 1/ejercicio 2.kt
+++ Practica 1/ejercicio 2.kt
@@ -1,5 +1,11 @@
+import kotlin.random.Random
 
-
+fun elegirOpcionComputadora(): String {
+    val opciones = listOf("Piedra", "Papel", "Tijera")
+    return opciones[Random.nextInt(opciones.size)]
+}
 fun main (){
+    val eleccionComputadora = elegirOpcionComputadora()
 
+    println("la computadora eligio: ${eleccionComputadora}")
 }
