cf0517e6c563a61498ddfd89eefdb61b6eede292
Kenny Luis Flores Chacón <kfloresc@ulasalle.edu.pe>
2024-08-25 23:25:49 -0500

    mejorando las impresiones en pantalla, dando pistas al usuario para que pueda adivinar el numero y mostrar los intentos restantes, ademas de agregar condicional para que no se elija un numero fuera de rango

--- Practica 1/ejercicio 4.kt
+++ Practica 1/ejercicio 4.kt
@@ -11,15 +11,24 @@ fun main(){
     while(intentos>0){
         println("Ingresa tu numero: ")
         val numero = readLine()?.toIntOrNull()
+        if (numero == null || numero !in 1..30) {
+            println("Por favor, ingresa un número válido entre 1 y 30.")
+            continue
+        }
 
         if(numero == numeroRandom){
             println("Felicidades! haz ganado")
             break
         }
-        else{
-            println("Intente de nuevo")
+        else if(numero< numeroRandom){
+            println("El número es mayor que $numero.")
         }
+        else {
+            println("El numero es menor que $numero")
+        }
+
         intentos--
+        println("Te quedan $intentos intentos! ")
     }
 
     println(numeroRandom)
