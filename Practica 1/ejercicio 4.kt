c42fac1d7048f3e5cb279ff1c328430cd9868d3e
Kenny Luis Flores Chacón <kfloresc@ulasalle.edu.pe>
2024-08-25 23:17:59 -0500

    añadiendo solicitud al usuario para ingresar su numero y un bucle que itera sobre la cantidad de intentos restantes que verifica si el numero ingresado es igual al numero random

--- Practica 1/ejercicio 4.kt
+++ Practica 1/ejercicio 4.kt
@@ -6,5 +6,21 @@ fun generarNumeroAleatorio ():Int{
 
 fun main(){
     val numeroRandom = generarNumeroAleatorio()
+    var intentos = 5
+
+    while(intentos>0){
+        println("Ingresa tu numero: ")
+        val numero = readLine()?.toIntOrNull()
+
+        if(numero == numeroRandom){
+            println("Felicidades! haz ganado")
+            break
+        }
+        else{
+            println("Intente de nuevo")
+        }
+        intentos--
+    }
+
     println(numeroRandom)
 }
