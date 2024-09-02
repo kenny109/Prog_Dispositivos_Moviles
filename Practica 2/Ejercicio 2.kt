49b65574edffcc73579e81c36923074f10ada358
Kenny Luis Flores Chacón <kfloresc@ulasalle.edu.pe>
2024-09-01 21:39:54 -0500

    implementando metodos get y set tanto para el precio como para el descuento

--- Practica 2/Ejercicio 2.kt
+++ Practica 2/Ejercicio 2.kt
@@ -4,5 +4,27 @@
 // Fecha última modificación: 01/09/2024
 
 class Producto(private var precio: Double, private var descuento: Double) {
-    // Constructor básico y propiedades iniciales
+    fun getPrecio(): Double {
+        return precio
+    }
+
+    fun setPrecio(nuevoPrecio: Double) {
+        if (nuevoPrecio >= 0) {
+            precio = nuevoPrecio
+        } else {
+            println("El precio no puede ser negativo.")
+        }
+    }
+
+    fun getDescuento(): Double {
+        return descuento
+    }
+
+    fun setDescuento(nuevoDescuento: Double) {
+        if (nuevoDescuento in 0.0..100.0) {
+            descuento = nuevoDescuento
+        } else {
+            println("El descuento debe estar entre 0 y 100%.")
+        }
+    }
 }
