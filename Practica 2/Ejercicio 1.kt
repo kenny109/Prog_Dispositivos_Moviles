daaddc04e445645a71b0ac7ac74b3bdfddab7e52
Kenny Luis Flores Chacón <kfloresc@ulasalle.edu.pe>
2024-09-01 20:23:19 -0500

    añadiendo descripción del ejercicio y creando clase basica de cuenta bancaria

---
+++ Practica 2/Ejercicio 1.kt
@@ -0,0 +1,8 @@
+// Descripción: Clase para representar una cuenta bancaria con saldo y límite de retiro.
+// Autor: Kenny Luis Flores Chacón
+// Fecha creación: 01/09/2024
+// Fecha última modificación: 01/09/2024
+
+class CuentaBancaria(private var saldo: Double, private var limiteRetiro: Double) {
+    // Constructor básico y propiedades iniciales
+}
