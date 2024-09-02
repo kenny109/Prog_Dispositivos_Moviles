c961c887becfb2ce5d384ca34bce251f324acd8e
Kenny Luis Flores Chacón <kfloresc@ulasalle.edu.pe>
2024-09-01 21:36:06 -0500

    creando clase producto

---
+++ Practica 2/Ejercicio 2.kt
@@ -0,0 +1,8 @@
+// Descripción: Clase para representar un producto con precio y descuento aplicable.
+// Autor: Kenny Luis Flores Chacón
+// Fecha creación: 01/09/2024
+// Fecha última modificación: 01/09/2024
+
+class Producto(private var precio: Double, private var descuento: Double) {
+    // Constructor básico y propiedades iniciales
+}
