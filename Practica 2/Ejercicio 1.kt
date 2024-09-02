718aa9da8f61b5e89a5073f0bdd14149974f004f
Kenny Luis Flores Chacón <kfloresc@ulasalle.edu.pe>
2024-09-01 20:26:47 -0500

    añadiendo funciones get y set con una condicional de que el nuevo saldo no puede ser menor a 0

--- Practica 2/Ejercicio 1.kt
+++ Practica 2/Ejercicio 1.kt
@@ -4,5 +4,16 @@
 // Fecha última modificación: 01/09/2024
 
 class CuentaBancaria(private var saldo: Double, private var limiteRetiro: Double) {
-    // Constructor básico y propiedades iniciales
+    //Metodo get para obtener el saldo
+    fun getSaldo(): Dobble{
+        return saldo
+    }
+    //Metodo set para establecer un nuevo saldo
+    fun setSaldo(nuevoSaldo: Double){
+        if (nuevoSaldo >= 0) {
+            saldo = nuevoSaldo
+        } else {
+            println("El saldo no puede ser negativo")
+        }
+    }
 }
