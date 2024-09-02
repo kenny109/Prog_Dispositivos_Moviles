5670f13201200135b08cc0fc7f1d536f17ecb167
Kenny Luis Flores Chacón <kfloresc@ulasalle.edu.pe>
2024-09-01 23:02:34 -0500

    creando subclase revista que sobreescribe el metodo mostrar detalles para mostrar los datos de la revista

--- Practica 2/Ejercicio 4.kt
+++ Practica 2/Ejercicio 4.kt
@@ -27,3 +27,24 @@ class Libro(
         println("Número de Páginas: $numeroPaginas")
     }
 }
+
+class Revista(
+    titulo: String,
+    autor: String,
+    anioPublicacion: Int,
+    val issn: String,
+    val volumen: Int,
+    val numero: Int,
+    val editorial: String
+) : Material(titulo, autor, anioPublicacion) {
+
+    override fun mostrarDetalles() {
+        println("Revista: $titulo")
+        println("Autor: $autor")
+        println("Año de Publicación: $anioPublicacion")
+        println("ISSN: $issn")
+        println("Volumen: $volumen")
+        println("Número: $numero")
+        println("Editorial: $editorial")
+    }
+}
