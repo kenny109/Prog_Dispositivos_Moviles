e4d0d4919286a2e7d2ae16974e2f5c7a70223065
kennymen <kfloresc@ulasalle.edu.pe>
2024-09-30 12:38:53 -0500

    implementando activity main para el manejo del fragment audiolist, ademas de crear clase audioitem para almacenar los valores de las canciones

---
+++ Practica 5/Ejercicio 1/app/src/main/java/com/example/p5_ejercicio1/AudioItem.kt
@@ -0,0 +1,8 @@
+package com.example.p5_ejercicio1
+
+data class AudioItem {
+    val name: String,
+    val image: Int,
+    val audioFile: Int,
+    val duration: String
+}
---
+++ Practica 5/Ejercicio 1/app/src/main/java/com/example/p5_ejercicio1/AudioListFragment.kt
@@ -0,0 +1,4 @@
+package com.example.p5_ejercicio1
+
+class AudioListFragment {
+}
