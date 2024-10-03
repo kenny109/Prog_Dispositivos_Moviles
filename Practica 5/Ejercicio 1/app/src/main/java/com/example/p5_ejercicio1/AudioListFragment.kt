3fba65cab205a7729d8ea743f7add80cb27a07bb
kennymen <kfloresc@ulasalle.edu.pe>
2024-10-03 00:11:38 -0500

    implementando el fragmento audio list para la muestra de los audios disponibles

--- Practica 5/Ejercicio 1/app/src/main/java/com/example/p5_ejercicio1/AudioListFragment.kt
+++ Practica 5/Ejercicio 1/app/src/main/java/com/example/p5_ejercicio1/AudioListFragment.kt
@@ -1,4 +1,43 @@
 package com.example.p5_ejercicio1
 
-class AudioListFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class AudioListFragment : Fragment() {
+
+    private lateinit var audioAdapter: AudioAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_audio_list, container, false)
+
+        val audioList = listOf(
+            AudioItem("Alan Walker - Faded", R.drawable.audio_image_1, R.raw.alan_walker_faded, "3:32"),
+            AudioItem("The Weeknd - Blinding Lights", R.drawable.audio_image_2, R.raw.the_weeknd_blinding_lights, "4:22"),
+            AudioItem("The Weeknd - Save Your Tears", R.drawable.audio_image_3, R.raw.the_weeknd_save_your_tears, "4:09"),
+            AudioItem("Daddy Yankee - Con Calma", R.drawable.audio_image_4, R.raw.daddyyankee_con_calma, "3:30"),
+            AudioItem("Ed Sheeran - Shape of You", R.drawable.audio_image_5, R.raw.ed_sheeran_shape_of_you, "3:55")
+        )
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.audio_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        audioAdapter = AudioAdapter(audioList) { audioItem ->
+            val fragment = AudioPlayerFragment.newInstance(audioItem.name, audioItem.image, audioItem.audioFile)
+            fragmentManager?.beginTransaction()
+                ?.replace(R.id.fragment_container, fragment)
+                ?.addToBackStack(null)
+                ?.commit()
+        }
+        recyclerView.adapter = audioAdapter
+
+        return view
+    }
 }
